b1ea5d8ca4037bca20f98b6793a51ac80bceec5b
tegshee <tegshbayar@gmail.com>
2020-08-05 14:12:02 -0700

    Set-up board and initial dev env

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// UI in Command Line
+// Board
+// whose turn
+// prompt to ask user input
+
+const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+console.log(`
+  ${board[0]} | ${board[1]} | ${board[2]}
+  ----------
+  ${board[0]} | ${board[1]} | ${board[2]}
+  ----------
+  ${board[0]} | ${board[1]} | ${board[2]}
+  `
+);
