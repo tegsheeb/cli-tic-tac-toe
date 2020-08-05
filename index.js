1236464511a663cca03b5ffcfe66da9ea41cb5b7
tegshee <tegshbayar@gmail.com>
2020-08-05 14:35:27 -0700

    Implement user input prompt into board

--- index.js
+++ index.js
@@ -3,13 +3,33 @@
 // whose turn
 // prompt to ask user input
 
+var prompt = require('prompt');
+
 const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-console.log(`
-  ${board[0]} | ${board[1]} | ${board[2]}
-  ----------
-  ${board[0]} | ${board[1]} | ${board[2]}
-  ----------
-  ${board[0]} | ${board[1]} | ${board[2]}
-  `
-);
+const getBoard = () => {
+  console.log(`
+    ${board[0]} | ${board[1]} | ${board[2]}
+    ----------
+    ${board[3]} | ${board[4]} | ${board[5]}
+    ----------
+    ${board[6]} | ${board[7]} | ${board[8]}
+    `
+  );
+}
+
+const whoseTurn = 'X';
+
+const getUserInput = (whoseTurn) => {
+  prompt.start();
+
+  prompt.get(['locationIndex'], function (err, result) {
+    board[result.locationIndex - 1] = whoseTurn;
+    getBoard();
+
+  });
+}
+
+getUserInput(whoseTurn);
+
+
